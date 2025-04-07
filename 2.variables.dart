435557ea9dd3cc065453147a196cf0c71d8cb096
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-07 10:57:31 +0530

    Update 2.variables.dart
    variables in dart

--- 2.variables.dart
+++ 2.variables.dart
@@ -1,8 +1,44 @@
-// variables in dart 
+void main() {
+  
+  
+  // rules for creating Variables
+  // keywords are not allowed
+  // only use alphabets numbers And $
+  // Space is Not allowed
+  // Number is not allowed as First Character
+  
+  var num = 22;
+  print(num);
+  
+  
+  // Variable Creation
+  
+  
+  // 1. Statically typed
+  
+  int value1 = 233;
 
-void main(){
-  var age = 13;
-  var name = "Shaurya";
- print(name);
- print(age);
+   String name = "Shaurya";
+  
+   double height = 5.33;
+   
+   bool kum  = true;
+   
+   print(value1);
+   print(name);
+   print(height);
+   print(kum);
+  // 2. Inferred Variable
+  
+ 
+
+  
+  
+  
+  
+  
+  // automaticall take set variable typed
+  
+  
+  
 }
