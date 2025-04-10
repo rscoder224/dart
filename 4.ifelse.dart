7be5f43102dbd43a66b3fa53dc8447f555458142
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-10 10:36:04 +0530

    Create 4.ifelse.dart
    dart if else

---
+++ 4.ifelse.dart
@@ -0,0 +1,21 @@
+void main() {
+  var x = 12;
+  print(x);
+  
+  int n = 21;
+  
+  print(n);
+  
+  String name = "Shaurya";
+  print(name);
+  
+  
+  if(name == "Shaurya"){
+    print("yes Name is true");
+  }else{
+    print("name is not true");
+  }
+  
+  
+  print("hello bro how are you ");
+}
